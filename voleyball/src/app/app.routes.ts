809ccf1df97ffb9485e4f3ce457fc37398acb3ad
swerliii <anastasiyavitryk@gmail.com>
2025-10-20 08:58:09 +0300

    add rules-component

--- voleyball/src/app/app.routes.ts
+++ voleyball/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import { RulesComponent } from './rules-component/rules-component';
 
 export const routes: Routes = [
-  { path: 'history', component: History }
+  { path: 'rules', component: RulesComponent }
 ];
---
+++ voleyball/src/app/rules-component/rules-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rules-component',
+  imports: [],
+  templateUrl: './rules-component.html',
+  styleUrl: './rules-component.css'
+})
+export class RulesComponent {
+
+}
