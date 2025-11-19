f564b881c6e061a359d3fc7aab49dd5dab5a3f30
ccarolinoss <carolina.karolina12@gmail.com>
2025-11-19 13:48:43 +0200

    Додали коипонент турнір

--- voleyball/src/app/app.routes.ts
+++ voleyball/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RulesComponent } from './rules-component/rules-component';
+import { TurnirComponent } from './turnir-component/turnir-component';
 
 export const routes: Routes = [
-  { path: 'rules', component: RulesComponent }
+  { path: 'rules', component: RulesComponent },
+  { path: 'turnir', component: TurnirComponent }
 ];
---
+++ voleyball/src/app/turnir-component/turnir-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-turnir-component',
+  imports: [],
+  templateUrl: './turnir-component.html',
+  styleUrl: './turnir-component.css',
+})
+export class TurnirComponent {
+
+}
