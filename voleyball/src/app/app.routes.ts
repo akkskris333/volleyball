0ce24cfb833b6b027c7b8641927c192bc5b6de8e
akkskris333 <kristina14879@gmail.com>
2025-11-24 09:16:46 +0200

    Add history component

--- voleyball/src/app/app.routes.ts
+++ voleyball/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { HistoryComponent } from './history-component/history-component';
 import { RulesComponent } from './rules-component/rules-component';
 import { TurnirComponent } from './turnir-component/turnir-component';
 
 export const routes: Routes = [
   { path: 'rules', component: RulesComponent },
-  { path: 'turnir', component: TurnirComponent }
+  { path: 'turnir', component: TurnirComponent },
+  { path: 'history', component: HistoryComponent }
 ];
